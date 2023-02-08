2eedc29358f779c489c47b410224d0aaec123fd4
Neefs <69609852+Neefs@users.noreply.github.com>
2023-02-08 15:53:25 -0500

    cleared

--- utils/errors.py
+++
@@ -1,6 +0,0 @@
-from discord.ext.commands import CommandError
-
-
-class NotDeveloper(CommandError):
-    def __init__(self):
-        super().__init__("You need to be a developer to run that command.")
